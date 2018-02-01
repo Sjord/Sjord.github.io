48e94b508a5c1962fad0cc5c385f753db7940eec
Sjoerd <sjoerd-github@linuxonly.nl>
2018-02-01 22:45:06 +0100

    Add demo page
    That makes bitlip attack possible.

---
+++ _demo/bitflip.php
@@ -0,0 +1,95 @@
+<?php
+$iv = bin2hex("circumnavigation");
+$key = "whatchamacallits";
+
+if (isset($_POST['username'])) {
+    $_POST['is_admin'] = 0;
+    $json = json_encode($_POST);
+    $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $json, MCRYPT_MODE_CBC, $iv);
+    header('Location: /bitflip.php?data='.bin2hex($ciphertext));
+}
+
+if (isset($_GET['data'])) {
+    $ciphertext = hex2bin($_GET['data']);
+    $plaintext = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $ciphertext, MCRYPT_MODE_CBC, $iv);
+    $plaintext = preg_replace('/[\x00-\x1F\x7F-\xFF]/', '', $plaintext);
+    $data = json_decode($plaintext);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <style type="text/css">
+      label, input {
+        display: block;
+      }
+      input {
+        margin-bottom: 1em;
+        width: 200px;
+        box-sizing: border-box;
+      }
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: sans-serif;
+        background-color: #312;
+      }
+      html, body {
+        height: 99%;
+      }
+      form {
+        background-color: white;
+        padding: 2em;
+        border: solid black 2px;
+      }
+      aside {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        border-bottom: solid black 1px;
+        background-color: #fcc;
+        padding: 1em;
+        text-align: center;
+      }
+      .loggedin, a {
+        color: white;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <?php
+        if (isset($plaintext)) {
+            if (is_null($data)) {
+                echo '<aside role="alert">Could not decode JSON</aside>';
+            } else if ($data->is_admin) {
+                echo '<div class="loggedin"><h1>Welcome, administrator!</h1></div><!--';
+            } else if ($data->username) {
+                echo '<aside role="alert">You are no administrator, '.htmlentities($data->username).'</aside>';
+            }
+        }
+    ?>
+    <form method="POST">
+      <label for="username">username</label>
+      <input type="username" id="username" name="username" placeholder="username" required autocomplete="username" autofocus>
+      <label for="nickname">nickname</label>
+      <input type="text" id="nickname" name="nickname" placeholder="nickname">
+      <label for="nickname">hobbies</label>
+      <input type="text" id="hobbies" name="hobbies" placeholder="hobbies">
+      <input type="submit" value="Submit">
+    </form>
+    <script>
+        let data = <?php echo $plaintext; ?>;
+        for (let key in data) {
+            let elem = document.getElementById(key);
+            if (elem) {
+                elem.value = data[key];
+            }
+        }
+    </script>
+    <!-- -->
+  </body>
+</html>
